1d8fe3e2a838cbc5b76768786adfbef4538388df
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2018-01-11 00:38:30 -0500

    Render Items inside ItemList container

--- src/ItemList/index.jsx
+++ src/ItemList/index.jsx
@@ -1,18 +1,39 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Item from "../Item";
 
 type Props = {
-  items: Array<Item>
+  items: Array<Item>,
+  selectedItems: Set<Item>
 };
 
 class ItemList extends Component<Props> {
+  componentWillMount = () => {
+    this.selectedItems = new Set();
+  };
+
+  toggleItem = item => {
+    if (this.selectedItems.has(item)) {
+      this.selectedItems.delete(item);
+    } else {
+      this.selectedItems.add(item);
+    }
+  };
+
   render() {
     const { items } = this.props;
     return (
       <div>
-        {items.map(item => <div key={item.value}> {item.label} </div> )}
+        {items.map(item => (
+          <Item
+            key={item.value}
+            type="checkbox"
+            item={item}
+            handleCheckboxChange={this.toggleItem}
+          />
+        ))}
       </div>
-    )
+    );
   }
 }
 
